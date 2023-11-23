e1e7ff4969ab20db607283fc85c0df48c9ddbeda
anestiandinesta <147995293+anestiandinesta@users.noreply.github.com>
2023-11-23 10:14:35 +0700

    Add files via upload

---
+++ kampus-app/kampus-app/src/model/Mahasiswa.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String prodi; // Tambah atribut prodi
+
+    public Mahasiswa(String nim, String nama, String prodi) {
+        this.nim = nim;
+        this.nama = nama;
+        this.prodi = prodi; // Inisialisasi atribut prodi
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+}
